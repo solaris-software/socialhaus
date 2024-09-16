4eb8e999f5a89cdb70d3d8cffaa9f947690c79aa
marcocastagnaro <marco.castagnaro@ing.austral.edu.ar>
2024-09-16 00:12:49 -0300

    effect in choose us

--- src/components/choose/index.jsx
+++ src/components/choose/index.jsx
@@ -1,50 +1,57 @@
 import styles from "./styles.module.scss";
-import team from "../../assets/team.png"
-import {Title} from "../common/title/index.jsx";
-import {motion} from "framer-motion";
+import { Title } from "../common/title/index.jsx";
+import { motion } from "framer-motion";
 
 const aboutContent1 = "Somos creadoras de estrategias que resuenan con tu marca. Nos movemos en la intersección de la creatividad y lo social.";
 const aboutContent2 = "El verdadero poder reside en comprender el comportamiento humano. Saber cómo las personas se conectan, se comunican y toman decisiones en un entorno social.";
 const aboutContent3 = "Combinamos nuestros conocimientos para ofrecerte un enfoque integral, analizando cada aspecto de tu marca, para garantizar que cada estrategia no solo sea creativa, sino también efectiva y diferenciadora.";
-const aboutContent4 = "No creemos en las soluciones genéricas; creemos en la personalización y en lo que ocurre cuando entendemos lo que hace única a tu marca."
-const aboutContent5 = "Queremos convertirnos en tus aliadas, tus confidentes y, sobre todo, en tus compañeras en este viaje creativo."
-const paragraphContainerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-        opacity: 1,
+const aboutContent4 = "No creemos en las soluciones genéricas; creemos en la personalización y en lo que ocurre cuando entendemos lo que hace única a tu marca.";
+const aboutContent5 = "Queremos convertirnos en tus aliadas, tus confidentes y, sobre todo, en tus compañeras en este viaje creativo.";
+
+const movingVariants = {
+    animate: (index) => ({
+        x: ["100%", "-100%"],
         transition: {
-            staggerChildren: 1
-        }
-    }
+            repeat: Infinity,
+            repeatType: "reverse",
+            duration: Math.random() * 15 + 5,
+            ease: "linear",
+            delay: Math.random() * index * 0.1,
+        },
+    }),
 };
 
-const paragraphVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 1 } }
-};
 export const Choose = () => {
+    const contents = [
+        aboutContent1,
+        aboutContent2,
+        aboutContent3,
+        aboutContent4,
+        aboutContent5,
+    ];
+
     return (
-        <div id={'team'} className={styles.container}>
+        <div id={"team"} className={styles.container}>
             <div className={styles.titleContainer}>
-                <Title title="WHY CHOOSE US" variant="light"/>
+                <Title title="WHY CHOOSE US" variant="light" />
             </div>
             <div className={styles.content}>
                 <div className={styles.leftContainer}>
-                    <motion.div
-                        initial="hidden"
-                        whileInView="visible"
-                        viewport={{once: true}}
-                        variants={paragraphContainerVariants}
-                    >
-                        <motion.p variants={paragraphVariants}>{aboutContent1}</motion.p>
-                        <motion.p variants={paragraphVariants}>{aboutContent2}</motion.p>
-                        <motion.p variants={paragraphVariants}>{aboutContent3}</motion.p>
-                        <motion.p variants={paragraphVariants}>{aboutContent4}</motion.p>
-                        <motion.p variants={paragraphVariants}>{aboutContent5}</motion.p>
+                    <motion.div>
+                        {contents.map((content, index) => (
+                            <motion.p
+                                key={index}
+                                className="word-moving"
+                                custom={index} // Pasar el índice como prop personalizada
+                                variants={movingVariants}
+                                animate="animate"
+                            >
+                                {content}
+                            </motion.p>
+                        ))}
                     </motion.div>
                 </div>
-
             </div>
-            </div>
-    )
-}
+        </div>
+    );
+};
